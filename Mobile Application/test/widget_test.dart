74e0fdcaf29b4ff0f15f56879f51a3eeacc3cb9f
OmarAshraf <124597807+Omarashraf0-0@users.noreply.github.com>
2025-10-11 22:09:25 +0300

    placeholder test files

--- Mobile Application/test/widget_test.dart
+++ Mobile Application/test/widget_test.dart
@@ -10,10 +10,14 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:alnoran_mobile_application/main.dart';
 
+// Ensure that main.dart contains a class named MyApp.
+// Example:
+// class MyApp extends StatelessWidget { ... }
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AlNoranApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
