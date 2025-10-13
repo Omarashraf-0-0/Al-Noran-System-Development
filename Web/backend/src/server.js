2b28714ff43fac6f1173b5b3c96308010823d045
OmarAshraf <124597807+Omarashraf0-0@users.noreply.github.com>
2025-10-13 05:15:34 +0300

    Update server.js

--- Web/backend/src/server.js
+++ Web/backend/src/server.js
@@ -30,7 +30,7 @@ app.use('/', require('./routes/root'))
 app.use((req, res) => {
     res.status(404)
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname,  '..', '..', '..', 'frontend', 'views',  '404.html'))
+        res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'views',  '404.html'))
     } else if (req.accepts('json')) {
         res.json({ message: '404 Not Found' })
     } else {
