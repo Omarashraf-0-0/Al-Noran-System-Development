c6220bc595dcc7f3ddd9708e94cd32bff9d2f9ef
aly <ialy24405@gmail.com>
2025-10-28 00:56:06 +0300

    Add ACID request functionality with form and model integration

---
+++ Web/frontend/src/pages/ACIDRequestPage.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import BackgroundContainer from "../components/BackgroundContainer";
+import FormContainer from "../components/FormContainer";
+import ACIDRequestForm from "../components/ACIDRequestForm";
+import acidRequestSchema from '../models/acid'
+import { toast } from "react-hot-toast";
+import axios from "axios";
+
+const ACIDRequestPage = () => {
+	const handleACIDRequest = (formData) => {
+		console.log("ACID Request attempt:", formData);
+		console.log("API URL:", import.meta.env.VITE_API_URL);
+		
+	};
+
+	return (
+		<>
+			<Navbar />
+			<BackgroundContainer>
+				<FormContainer>
+					<ACIDRequestForm onSubmit={handleACIDRequest} />
+				</FormContainer>
+			</BackgroundContainer>
+		</>
+	);
+};
+
+export default  ACIDRequestPage;
