e9f73e68e1cf87691cd33c8d47ed8b5df21ec665
SALAH164 <116555702+SALAH164@users.noreply.github.com>
2025-12-02 00:10:17 +0200

    nothing

--- Web/backend/src/middleware/auth.js
+++ Web/backend/src/middleware/auth.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 const protect = async (req, res, next) => {
 	let token;
 
-	//return next(); 
+	return next(); 
 	// Check if authorization header exists and starts with Bearer
 	if (
 		req.headers.authorization &&
